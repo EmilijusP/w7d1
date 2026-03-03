72fd1c95f8ca74b53dc59bac15050acccdd74d1d
[Emilijus Pocius] <[emilijus.pocius@visma.com]>
2026-03-03 14:33:09 +0200

    base kernel setup

--- AnagramSolver/AgentDemo/Program.cs
+++ AnagramSolver/AgentDemo/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.SemanticKernel;
+using Microsoft.Extensions.Configuration;
+
+var config = new ConfigurationBuilder()
+    .AddUserSecrets<Program>()
+    .Build();
+
+var builder = Kernel.CreateBuilder();
+builder.AddOpenAIChatCompletion(
+    modelId: config["OpenAI:Model"]!,
+    apiKey: config["OpenAI:ApiKey"]!);
+
+var kernel = builder.Build();
+
+// Paprastas LLM kvietimas
+var result = await kernel.InvokePromptAsync(
+    "Kas yra Semantic Kernel? Atsakyk 2 sakiniais.");
+Console.WriteLine(result);
